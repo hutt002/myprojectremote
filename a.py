ac503da6c5c3168c37abd4773bed9e5f1f45154d
hutaotao <453444346@qq.com>
2022-07-02 15:43:53 +0800

    第二次提交

--- a.py
+++ a.py
@@ -1,2 +1,3 @@
 print("Hello world!")
 print("https://github.com/hutt002/myprojectremote.git")
+print("git@github.com:hutt002/myprojectremote.git")
---
+++ a.py.bak
@@ -0,0 +1,2 @@
+print("Hello world!")
+print("https://github.com/hutt002/myprojectremote.git")
